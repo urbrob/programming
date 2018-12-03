d6dce4514cd60d7910d2be4a6bdc7fb5a225bc20
Robert <urb.rob@o2.pl>
2018-12-04 00:28:50 +0100

    Done

--- Ruby/Zad1/lib/bottles.rb
+++ Ruby/Zad1/lib/bottles.rb
@@ -1,25 +1,26 @@
 class Bottles
 
-   def drink_or_buy(bootles)
+   def drink_or_buy(bottles)
      if bottles > 0
-       return 'Take it down and pass it around'
+       return "Take #{bottles == 1 ? 'it' : 'one'} down and pass it around"
      return 'Go to the store and buy some more'
+    end
    end
 
-   def sing_page(page)
-     <<-EOF
-          #{page != 0 ? page : 'No more'} bottles of beer on the wall, #{page != 0 ? page : 'no more'} of beer.
-          #{drink_or_buy(page)}, #{page != 0 ? page : 'no more'} of beer on the wall.
-        EOF
+   def verse(page)
+     return "#{page != 0 ? page : 'No more'} bottles of beer on the wall, #{page != 0 ? page : 'no more'} bottles of beer.
+#{drink_or_buy(page)}, #{page != 1 ? page - 1 : 'no more'} bottles of beer on the wall.\n"
    end
 
-   def verse(from_page, to_page=from_page)
+   def verses(from_page, to_page=from_page)
+     text_to_sing = ''
      for page in from_page..to_page do
-       sing_page(page)
+       text_to_sing << verse(page)
      end
+     return text_to_sing
    end
 
    def song
-    verse(99, 0)
+    return verse(99, 0)
    end
 end
