a29922b99d8f85c0b612df5657c5d0e72f565993
urb.rob <urb.rob@o2.pl>
2018-12-12 11:46:40 +0100

    adwa

--- C++/Assoc/test.cpp
+++ C++/Assoc/test.cpp
@@ -7,9 +7,8 @@ int main() {
     a1["ala"] = 1;
     a1["ola"] = 2;
     cout << a1["ala"] << " " << a1["ola"] << " " << a1["ula"] << endl;
-    a2 = a1;
-    a2["ula"]++;
-    cout << a2["Ala"] << " " << a2["oLa"] << " " << a2["uLa"] << endl;
+    a1["ula"]++;
+    cout << a1["Ala"] << " " << a1["oLa"] << " " << a1["uLa"] << endl;
     
     cout << "KONIEC TESTU CASE SENSITIVE" << endl;
 
@@ -17,10 +16,10 @@ int main() {
     b1["ala"] = 3;
     b1["ola"] = 4;
     cout << b1["ala"] << " " << b1["ola"] << " " << b1["ula"] << endl;
-    b2 = b1;
-    b2["uLa"]++;
-    cout << b2["alA"] << " " << b2["oLa"] << " " << b2["ulA"] << endl;
-    cout << b2["ALA"] << " " << b2["olA"] << " " << b2["Ula"] << endl;
+    b1["uLa"]++;
+    b2["olada"]++;
+    cout << b1["alA"] << " " << b1["oLa"] << " " << b1["ulA"] << endl;
+    cout << b2["olada"] << " " << b2["olA"] << " " << b2["Ula"] << endl;
     cout << b1["dla"] << " " << b1["owa"] << " " << b1["uaa"] << endl;
     cout << "KONIEC TESTU NON CASE SENSITIVE" << endl;
     return 0;
