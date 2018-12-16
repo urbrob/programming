1e5cc984038985c41555a754d8a745f1d7288bc0
Robert <urb.rob@o2.pl>
2018-12-17 00:13:03 +0100

    Add map

--- C++/Map/Map.h
+++ C++/Map/Map.h
@@ -8,53 +8,73 @@ using namespace std;
 
 template <class ID_T, class T>
 class Map{
-	
+
 	struct record {
 	    record *next;
 	    ID_T id;
 	    T data;
-		
-		
+
+			record(const record &rec) {
+				id = rec.id;
+				next = rec.next;
+				data = rec.data;
+			}
+
 	    record(ID_T zid, T zdata):next(NULL){
 	        id = zid;
 	        data = zdata;
 	    };
+			
+			record &operator=(record rec){
+				record new_rec;
+				new_rec->id = rec.id;
+				new_rec->next = rec.next;
+				new_rec->data = rec.data;
+				return &new_rec;
+			}
 	};
-	
-	private:
-    	record & operator= (const record &);
+
 public:
 	record *head;
+
 	Map(){
 		head = NULL;
 	}
-	// If there is no record with given ID, return last record (NULL)
-	
-	record* find_record(ID_T id){
-		record *pom_head = head;
-		while(pom_head){
-			if(pom_head->id == id) return pom_head->next;
-			pom_head = pom_head->next;
+
+	T *find(const ID_T key){
+		record *table = head;
+		while (table){
+			if (key == table->id)
+				return &table->data;
+			table = table->next;
 		}
-		return pom_head;
+		return NULL;
 	}
-	
-	T* find(ID_T id){
-		T* pom = find_record(id)->data
-		return pom;
-	}
-	
+
 	void add(ID_T id, T data){
-		record *pom_record = find_record(id);
-		pom_record = new record(id, data);
+		if(!head) head = new record(id, data);
+		else {
+			record* pom = head;
+			while(pom){
+				if(pom->id == id){
+					delete &pom->data;
+					pom->data = data;
+					break;
+				}
+				if(!pom->next) break;
+				pom = pom->next;
+			}
+			pom->next = new record(id, data);
+		}
 	}
-	
+
 	friend ostream &operator<<(std::ostream &out, const Map& map) {
 		record *pom_head = map.head;
 		while(pom_head){
 			out << pom_head->id << " - " << pom_head->data << endl;
+			pom_head = pom_head->next;
 		}
-		return out<< " KUPA";
+		return out;
 	};
 
 };
